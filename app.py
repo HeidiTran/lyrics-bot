d5bc2879ecfe533cb124b8946cf1acbc4fb21767
st974 <st974@drexel.edu>
2020-04-12 13:04:48 -0400

    Added basic structure for Flask app

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask, request, render_template
+
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "GET":
+        return render_template("index.html")
+    else:
+        return "TO DOOOO"
